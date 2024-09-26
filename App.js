555a1c5ca976e808d03ae1b086b0531663305db0
Santosh Pawar <Santosh.P@Orioninc.com>
2024-09-26 17:02:50 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,20 @@
+const heading = React.createElement(
+  "div",
+  {
+    id: "parent",
+  },
+  [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I am h1 tag"),
+      React.createElement("h2", {}, "I am h2"),
+    ]),
+
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I am h1 tag"),
+      React.createElement("h2", {}, "I am h2"),
+    ]),
+  ]
+);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
